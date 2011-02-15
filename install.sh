dca4bc03f92a1637074451d513059fcf4a2240ba
Mark Szymanski <mrman208@me.com>
2011-02-15 16:30:58 -0600

    Install script now copies the image to the pianobar config directory

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ fi
 chmod +x growl.sh
 
 cp growl.sh ~/.config/pianobar/
+cp pandora-icon-cc-by-sa-rossr.png ~/.config/pianobar
 
 echo "event_command = $HOME/.config/pianobar/growl.sh" >> ~/.config/pianobar/config
 
